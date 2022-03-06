67aee2d01832cce7a8760b07520fdd507aa12eeb
Masanov <masanov3167@gmail.com>
2022-03-06 23:37:34 +0500

    gbnvjcfmkd

--- scripts/taskfour.js
+++ scripts/taskfour.js
@@ -5,5 +5,14 @@
 //     {name: "Vali", price: 2000},
 // ];
 
+// console.log(arr.name);
 
+// var arr = {
+//     name: "Ali", price: 300,
+//     name: "Iso", price: 500,
+//     name: "Umar", price: 100,
+//     name: "Vali", price: 2000,
+//     name: "Vali", price: 200,
+// };
 
+// console.log(arr.name);
